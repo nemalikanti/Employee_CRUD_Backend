e714568264f5d929ff9abc648eb9b693fc2e0866
nemalikanti <125137564+nemalikanti@users.noreply.github.com>
2023-03-09 17:41:18 +0530

    Delete emp_management directory

--- emp_management/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emp_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.emp_management.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	 
-}
