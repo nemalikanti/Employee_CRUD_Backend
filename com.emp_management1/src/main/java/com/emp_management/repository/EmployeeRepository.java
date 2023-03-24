b3456707a9e6255728d0eaddedbee46320109603
nemalikanti <125137564+nemalikanti@users.noreply.github.com>
2023-03-24 13:13:55 +0530

    Update EmployeeRepository.java

--- com.emp_management1/src/main/java/com/emp_management/repository/EmployeeRepository.java
+++ com.emp_management1/src/main/java/com/emp_management/repository/EmployeeRepository.java
@@ -1,9 +1,19 @@
 package com.emp_management.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.emp_management.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	 
+
+	//Custom query
+	 @Query(value = "select * from Employee e where e.name like %:key% or e.email like %:key%", nativeQuery = true)
+	 List<Employee> findByKey(@Param("key") String key);
+	
+//	List<Employee> searchByName(String name);
+
 }
